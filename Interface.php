260b222191340febcb6c064f48e21e25c420bfe4
mahammadirfan2002 <110482281+mahammadirfan2002@users.noreply.github.com>
2023-02-28 11:38:36 +0530

    Add files via upload

---
+++ Interface.php
@@ -0,0 +1,352 @@
+<html>
+  <head>
+    <title>
+        Special case
+    </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        
+        .table{
+            border:120px solid blue;
+            display:inline-block;
+            width:25%;
+        }
+        body{
+            background: fixed;
+        }
+            div.b {
+             display: block;
+            width: 23%;
+             height: 50%;
+             padding: 15px;
+             border: 10px solid violet;    
+             border-radius:19px;
+             margin: 2%;
+             background-color: red; 
+             position:relative;
+             top:270px;
+             left:36%;
+}
+@media only screen and (max-width:770px)
+            {
+                .maintable{
+                   flex-direction: column;
+                   position:absolute;
+                   top: 1%;
+                   left: 20%;
+                   width: 10%;
+                   height: 10%;
+                   padding: 5px;
+                    border: 1px solid blue;    
+                    background-color: yellow; 
+                }
+                body{
+                    overflow-x: hidden;
+
+                }
+            }
+            /*body{
+                background-image: url('Background.jpg');
+                background-repeat: no-repeat;
+                background-position: 10% 5%;
+                background-size: 1800px 890px;
+            }*/
+            #l{
+                
+                position: absolute;
+                top: 10px;
+                left: 7px;
+                width: 99%;
+                height: 98%;
+                border-radius: 25px;
+            }
+            .college{
+                width:100%;
+                height:50%;
+                border-radius:12px;
+                
+            }
+            p{
+                font-size:31px;
+                color:yellow;
+            }
+            .other{
+                display: block;
+            width: 23%;
+             height: 50%;
+             padding: 15px;
+             border: 10px solid violet;    
+             border-radius:19px;
+             margin: 2%;
+             position:absolute;
+             bottom: 1px;
+             left:68%;
+                background-color:blue;
+                /*position: relative;
+                left: 50px;*/
+                
+            }
+            .c{
+                width: 100%;
+                height: 50%;
+                border-radius: 13px;
+            }
+            h1{
+                width: 50%;
+                position: absolute;
+                top: 100px;
+                left: 50%;
+                font-size: 55px;
+            }
+            button{
+                position: absolute;
+                top: 28%;
+                left: 58% ;
+                font-size: 40px;
+                border-radius: 15px;
+            }
+            button:hover{
+                background-color:skyblue;
+            }
+            button.c3{
+                position: absolute;
+                top:30px;
+                left: 40%;
+                border-radius: 12px;
+                border: none;background: transparent;
+            }
+           .c5{
+            position: absolute;
+                top:30px;
+                left: 51%;
+                text-decoration: none;
+                font-size: 40px;
+                color: black;
+
+           }
+           .c6{
+            position: absolute;
+                top:30px;
+                left: 61%;
+                text-decoration: none;
+                font-size: 40px;
+                color: black;
+
+           }
+           .c7{
+            position: absolute;
+                top:30px;
+                left: 69%;
+                text-decoration: none;
+                font-size: 40px;
+                color: black;
+
+           }
+           /*.c{
+            text-decoration: none;
+            position: absolute;
+            top: 1500px;
+            right: 1500px;
+           }*/
+           .c8{
+                position: absolute;
+                top: 1950px;
+                left: 50px;
+                background: transparent;
+                opacity:1;
+                font-size: 50px;
+                border:none;
+                text-decoration: none;
+                color: black;
+            }
+            .c9{
+                position: absolute;
+                top: 2000px;
+                left: 70px;
+                background: transparent;
+                opacity:1;
+                font-size: 30px;
+                border:none;
+                text-decoration: none;
+                color: black;
+            }
+            .c10{
+                position: absolute;
+                top: 2430px;
+                right:1350px;
+                background: transparent;
+                opacity:1;
+                font-size: 50px;
+                border:none;
+                text-decoration: none;
+                color: black;
+            }
+            .c11{
+                position: absolute;
+                top: 1210px;
+                left: 70px;
+                background: transparent;
+                opacity:1;
+                font-size: 30px;
+                border:none;
+                text-decoration: none;
+                color: black;
+            }
+            .c12{
+                position: absolute;
+                top: 2650px;
+                right:1300px;
+                background: transparent;
+                opacity:1;
+                font-size: 50px;
+                border:none;
+                text-decoration: none;
+                color: black;
+            }
+            .c13{
+                position: absolute;
+                top: 2870px;
+                left: 70px;
+                background: transparent;
+                opacity:1;
+                font-size: 30px;
+                border:none;
+                text-decoration: none;
+                color: black;
+            }
+            .space{
+                font-size:30px;
+                padding: 2%;
+            }
+            .c14{
+                position: absolute;
+                top:30px;
+                left: 83%;
+                text-decoration: none;
+                font-size: 40px;
+                color: black;
+
+            }
+    </style>
+  </head>
+  <body style="background-image: url('Background.jpg') ; background-repeat: 2;background-position: 10% 5%;background-size: 1800px 890px;">
+  <img alt="icon" id="l" src="imag4.jpg"></img>
+     <div class="maintable">
+     
+     <div> 
+        <div class="b">
+        <img class="college" src="IIT_Karagpur.jpg">
+        <p class="college">To know the information uploaded by IIT KARAGPUR college click here</p>
+        </div> 
+        <div class="b">
+        <img class="college" src="IIT Tirupati.jpeg">
+        <p class="college">To know the information uploaded by IIT TIRUPATI college click here</p>
+        </div> 
+        <div class="b">
+        <img class="college" src="IIT Madras.jpeg">
+        <p class="college">To know the information uploaded by IIT MADRAS college click here</p>
+        </div> 
+    </div>
+    <div class="other"> 
+        <div class="a">
+        <img class="c" src="IIT Tirupati.jpeg">
+        <p class="c">To know the information uploaded by IIT TIRUPATI college click here</p>
+        </div> 
+        <div class="a">
+        <img class="c" src="IIT Madras.jpeg">
+        <p class="c">To know the information uploaded by IIT MADRAS college click here</p>
+        </div> 
+        <div class="a">
+        <img class="c" src="IIT_Karagpur.jpg">
+        <p class="c">To know the information uploaded by IIT KARAGPUR college click here</p>
+        </div>
+    </div>
+    </div>
+    <h1>Education For Everyone</h1>
+    <button>Click For Events</button>
+    
+    <h2><b><a id="f" href="C:\Users\mdres\OneDrive\Desktop\PROJECT1\Student_organisation.html">Login/Register</a></b></h2>
+        <button class="c3"><svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+            <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+          </svg><b>Home</b></button>
+          <b><a class="c5" href=#About>About</a></b>
+          <b><a class="c6" href=#help>Help</b></a>
+          <b><a class="c7" href=#contactus>Contact Us</b></a>
+    <!--h1><a class="c8" href=#About>About</a></h1-->
+          <b><a class="c8" name=About>About</a></b>
+          <div class="c9">
+          <ul>
+            <li>
+                Teachers, students are encouraged to take up research and innovate.
+            </li>
+            <li>Current accreditation, ranking and grading norms emphasizes research work and publications by both students and faculty.</li>
+            <li>In order to take up research work and subsequently make publications of research findings, to collaborate and network with others who are working on similar problems there is a need to participate in global, national and local workshops, seminars, symposia, conferences, etc. </li>
+            <li> Availability of single portal will be extremely useful for obtaining the information about all academic activities across the country, universities and international arena in chronological order and in subject areas so as to enable the students, faculty, and researchers to access information and prepare well in advance and participate effectively and successfully.</li>
+            <li>Lot of time and efforts are wasted in this regard and despite that they may miss out vital information and chance to make presentations</li>
+            <li> Addressing this problem will be extremely useful for obtaining the information about all academic activities across the country, universities and international arena in chronological order and in subject areas so as to enable the students, faculty, and researchers to access information and prepare well in advance and participate effectively and successfully.</li>
+          </ul>
+          </div>
+          <h1><a class="c10"  name=help>Help</a></h1>
+          <div class="c11">
+          <ul ><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+              <li>To know how to use The website.</li>
+              <li>This portal is used for knowing information of events or programs from other university or college.</li>
+              <li>By login as a student we can know more information directly from college to take participation in the events and programs.</li>
+          </ul></div>
+          <h1><a class="c12" name=contactus>Contact Us</a></h1>
+          <ul class="c13">
+          <li>For any quaries</li>
+            <li>Contact us through </li>
+            <li>
+                <table id="k">
+                    <tr>
+                        <td class="space">Phone Number:</td>
+                        <td></td>
+                        <td class="space">Email Id:</td>
+                        <td class="space">Name:</td>
+                    </tr>
+                    <tr>
+                        <td class="space">+918309972009</td>
+                        <td></td>
+                        <td class="space">gteam8588@gmail.com</td>
+                        <td class="space">Rahul</td>
+                    </tr>
+                </table>
+            </li>
+        </ul>
+        <h2><b><a class="c14" href="Student_Organisation.php">Login/Register</a></b></h2>
+
+    <script>
+             var myIndex = 0;
+carouse();
+function carouse() {
+  var i;
+  var x = document.getElementsByClassName("b");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "inline-block";  
+  setTimeout(carouse, 2000);}
+</script>
+<script>
+             var myvalue = 0;
+carousel();
+function carousel() {
+  var j;
+  var y = document.getElementsByClassName("a");
+  for (j = 0; j < y.length; j++) {
+    y[j].style.display = "none";  
+  }
+  myvalue++;
+  if (myvalue > y.length) {myvalue = 1}    
+  y[myvalue-1].style.display = "inline-block";  
+  setTimeout(carousel, 2000);}
+</script>
+  </body>
+
+
+</html>
